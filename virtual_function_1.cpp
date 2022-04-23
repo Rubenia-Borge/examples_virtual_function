8e18739d967c6db1794c6ae3fffd257826ff8f96
Rubenia Borge <rubeniaolaska@gmail.com>
2022-04-22 23:17:20 -0500

    Add files via upload

---
+++ virtual_function_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>  
+using namespace std;  
+class BaseClassA  
+{  
+   int baseData = 100;  
+   public:  
+     void display()  
+     {  
+        cout << "Data in base class: " << baseData << endl;  
+     }  
+};  
+
+class DerivedClassB: public BaseClassA  
+{  
+    int derivedData = 200;  
+    public:  
+    void display()  
+    {  
+        cout << "Data in derived class: " << derivedData << endl;  
+    }  
+};  
+int main()  
+{  
+    BaseClassA *baseClassObject;  // A pointer to BaseClassA
+    DerivedClassB derivedClassObject;  // An object of type DerivedClassB
+
+        
+    baseClassObject = &derivedClassObject;  
+    baseClassObject->display();  
+    return 0;  
+}  
